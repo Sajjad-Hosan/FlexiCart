cc3aa95b0d18e25d4bba2ee00cd15904ae96911d
Sajjad-Hosan <s4jj4dho34n@hmail.com>
2025-01-25 02:44:06 +0600

    dashboard overview page, dashboard navbar

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,54 +1,64 @@
 import { useState } from "react";
 import { AiOutlineProduct } from "react-icons/ai";
-import { GoHome } from "react-icons/go";
+import { FaUsersGear } from "react-icons/fa6";
+import { GoHome, GoListOrdered } from "react-icons/go";
+import { LiaToolsSolid } from "react-icons/lia";
 import { LuBellDot } from "react-icons/lu";
+import {
+  MdGridView,
+  MdOutlineBugReport,
+  MdOutlineDashboardCustomize,
+  MdOutlineNotificationsActive,
+  MdOutlinePayment,
+} from "react-icons/md";
+import { TbLayoutGridRemove, TbShoppingCartCopy } from "react-icons/tb";
 import { NavLink, Outlet } from "react-router-dom";
 
 const dashboardArrays = [
   {
     path: "/dashboard/overview",
     name: "Overview",
-    icon: <GoHome className="text-xl" />,
+    icon: <MdGridView className="text-xl" />,
   },
   {
     path: "/dashboard/products-management",
     name: "Product Management",
-    icon: <AiOutlineProduct className="text-xl" />,
+    icon: <TbShoppingCartCopy className="text-xl" />,
   },
   {
     path: "/dashboard/order-management",
     name: "Order Management",
-    icon: <GoHome className="text-xl" />,
+    icon: <GoListOrdered className="text-xl" />,
   },
   {
     path: "/dashboard/customer-management",
     name: "Customer Management",
-    icon: <AiOutlineProduct className="text-xl" />,
+    icon: <FaUsersGear className="text-xl" />,
   },
   {
     path: "/dashboard/sales-reports",
     name: "Sales & Reports",
-    icon: <GoHome className="text-xl" />,
+    icon: <MdOutlineBugReport className="text-xl" />,
   },
   {
     path: "/dashboard/payment-management",
     name: "Payment Management",
-    icon: <AiOutlineProduct className="text-xl" />,
+    icon: <MdOutlinePayment className="text-xl" />,
   },
   {
     path: "/dashboard/shipping-management",
     name: "Shipping Management",
-    icon: <GoHome className="text-xl" />,
+    icon: <TbLayoutGridRemove className="text-xl" />,
   },
   {
     path: "/dashboard/marketing-tools",
     name: "Marketing Tools",
-    icon: <AiOutlineProduct className="text-xl" />,
+    icon: <LiaToolsSolid className="text-xl" />,
   },
   {
     path: "/dashboard/notifications",
     name: "Notifications",
-    icon: <GoHome className="text-xl" />,
+    icon: <MdOutlineNotificationsActive className="text-xl" />,
   },
 ];
 const menuArrays = [
@@ -64,25 +74,37 @@ const menuArrays = [
     name: "Products",
     icon: <AiOutlineProduct className="text-xl" />,
   },
-  {
-    path: "/blog",
+  //   {
+  //     path: "/blog",
 
-    name: "Blogs",
-    icon: <LuBellDot className="text-xl" />,
-  },
+  //     name: "Blogs",
+  //     icon: <LuBellDot className="text-xl" />,
+  //   },
 ];
 const Dashboard = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="flex">
+    <div className="flex h-full overflow-hidden">
       <div
         className={`${
-          isOpen ? "w-[20%] justify-center items-center  p-6" : "w-[7%] p-3"
-        } bg-orange-500 h-full flex  flex-col gap-2 overflow-hidden`}
+          isOpen ? "w-[19%] justify-center items-center  p-4" : "w-[6%] p-3"
+        } bg-base-300 flex  flex-col gap-2 overflow-hidden transition-all duration-200 fixed`}
       >
-        <h1 className="text-3xl font-semibold">{isOpen ? "Dashboard" : "D"}</h1>
+        <div
+          className="mb-1 btn transition-all duration-200"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <>
+              <MdOutlineDashboardCustomize className="text-2xl" />
+              <p className="text-xl font-semibold">Dashboard</p>
+            </>
+          ) : (
+            <MdOutlineDashboardCustomize className="text-2xl" />
+          )}
+        </div>
 
-        <ul className="menu rounded-box w-56 gap-2">
+        <ul className="menu rounded-box w-56 gap-2 transition-all duration-200">
           {dashboardArrays.map(({ path, icon, name }, i) => (
             <li key={i}>
               <NavLink to={path}>
@@ -91,8 +113,8 @@ const Dashboard = () => {
             </li>
           ))}
         </ul>
-          <div className="mx-auto w-full bg-gray-500 h-[1px]"></div>
-        <ul className="menu w-56 gap-2">
+        <div className="mx-auto w-full bg-gray-500 h-[1px]"></div>
+        <ul className="menu w-56 gap-2 transition-all duration-200">
           {menuArrays.map(({ path, icon, name }, i) => (
             <li key={i}>
               <NavLink to={path}>
@@ -103,18 +125,25 @@ const Dashboard = () => {
         </ul>
         {isOpen ? (
           <>
-            <div className="card border p-2 flex-row items-center gap-4 w-full mt-2 transition-all duration-100">
+            <div className="card border border-dashed border-gray-500 p-2 flex-row items-center gap-4 w-full transition-all duration-200">
               <div className="avatar w-10 h-10 border rounded-full"></div>
               <h1 className="font-semibold">Sajjad Hosan</h1>
             </div>
           </>
         ) : (
           <>
-            <div className="card justify-center items-center border p-2 mt-4 avatar w-10 h-10 rounded-full mx-auto cursor-pointer transition-all duration-100 hover:scale-95 tooltip" data-tip='Sajjad'></div>
+            <div
+              className="card justify-center items-center border border-dashed border-gray-500 p-2 mt-4  w-10 h-14 rounded-full mx-auto cursor-pointer transition-all duration-100 hover:scale-95 tooltip"
+              data-tip="Sajjad"
+            ></div>
           </>
         )}
       </div>
-      <div className={`w-full h-full p-4 border`}>
+      <div
+        className={`${
+          isOpen ? "w-[81%]" : "w-[94%]"
+        } p-8 border transition-all duration-200 overflow-scroll ml-auto`}
+      >
         <Outlet />
       </div>
     </div>
--- src/Dashboard/DashboardsPages/Overview/Overview.jsx
+++ src/Dashboard/DashboardsPages/Overview/Overview.jsx
@@ -1,10 +1,37 @@
+import { MdOutlineDashboardCustomize } from "react-icons/md";
 
 const Overview = () => {
-    return (
-        <div>
-        <h1 className="text-4xl">This is Overview Page</h1>            
+  return (
+    <div className="space-y-4">
+      <div className="flex justify-between items-center  mb-10">
+        <div className="flex items-center gap-2">
+          <MdOutlineDashboardCustomize className="text-3xl" />
+          <p className="text-xl font-semibold">Dashboard Overview</p>
         </div>
-    );
+        <div className="flex items-center gap-2">
+            <button className="btn btn-circle btn-neutral btn-sm">A</button>
+            <button className="btn btn-circle btn-neutral btn-sm">A</button>
+            <button className="btn btn-circle btn-neutral btn-sm">A</button>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-8 w-ful">
+        <div className="card border border-dashed border-gray-500 p-4 h-[20rem]  flex-1">
+          Box 1
+        </div>
+        <div className="card border border-dashed border-gray-500 p-4 h-[20rem] flex-1">
+          Box 2
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-8 w-ful">
+        <div className="card border border-dashed border-gray-500 p-4 h-[20rem] flex-1">
+          Box 3
+        </div>
+        <div className="card border border-dashed border-gray-500 p-4 h-[20rem] flex-1">
+          Box 4
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
